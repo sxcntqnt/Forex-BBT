f814f99b9cda3838510e6a65cc50994ca40c558a
sxcntcnquntns <47719958+sxcntqnt@users.noreply.github.com>
2025-01-30 13:28:25 +0300

    Update config_write.py

--- config/config_write.py
+++ config/config_write.py
@@ -3,8 +3,8 @@ from configparser import ConfigParser
 config = ConfigParser()
 
 config.add_section('main')
-config.set('main', 'DERIV_API_TOKEN', 'MiykESZODk7qDiG')
-config.set('main', 'APP_ID', '36544')
+config.set('main', 'DERIV_API_TOKEN', '')
+config.set('main', 'APP_ID', '')
 
 with open(file='config/config.ini', mode='w') as f:
     config.write(f)
